64a1271bc5e51944821dd3836864fdb8c617b3a5
Nonthpat Wongkham <bnbn5588@hotmail.com>
2024-10-15 09:39:17 +0700

    fix alert

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+/* Metadata without template
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+*/
+// Metadata with template
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>{children}</body>
+    </html>
+  );
+}
